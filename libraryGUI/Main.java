28efc8311f86da2d5e5d6f133e619960911fc3b1
Affan-Amir <95192438+Affan-Amir@users.noreply.github.com>
2022-01-09 21:50:14 -0500

    Update Main.java

--- libraryGUI/Main.java
+++ libraryGUI/Main.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class Main{
   
   public static void main(String[] args) throws IOException{
-    
+    //The username to login is "Hello" (Capital H). The password is "world" (No Capital).
     finalAssignment finalAssignment = new finalAssignment();
  //Final assignment is the main file that will lead to opening all of the rest of the files   
   }
