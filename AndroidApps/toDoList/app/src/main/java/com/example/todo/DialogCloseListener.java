fc7837a208e8d6ef6073e9197510de5dbff76a7a
Affan-Amir <95192438+Affan-Amir@users.noreply.github.com>
2022-02-08 22:17:47 -0500

    Finished App
    Adding all classes required to run program. Xml and java files were used.

---
+++ AndroidApps/toDoList/app/src/main/java/com/example/todo/DialogCloseListener.java
@@ -0,0 +1,11 @@
+package com.example.todo;
+
+
+import android.content.DialogInterface;
+
+/**
+ * Interface that acts as a Dialog Listener
+ */
+public interface DialogCloseListener {
+    public void handleDialogClose(DialogInterface dialog);
+}
